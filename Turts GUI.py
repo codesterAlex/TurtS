3cc5f39b9ec345b0f3afedbcb63ef9a6d5412794
Kiran <pradhanangakiran@gmail.com>
2017-11-02 20:31:36 +0545

    Adding first function Button callback #working

--- Turts GUI.py
+++ Turts GUI.py
@@ -1,6 +1,17 @@
+import turtle
 from tkinter import *
 
+def showturtle():       #function for showing turtle
+    turtle.showturtle()
+
 window = Tk()
+window.title("Turtle")
+window.geometry("300x300")
+
 heading = Label(window, text = "TURTLE GUI")
+
+showturtle_button = Button(window, text = "Show Turtle", command = showturtle)
+
+showturtle_button.pack()
 heading.pack()
 window.mainloop()
