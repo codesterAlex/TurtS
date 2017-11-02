a7b8b73a488295f8398895a5aeb8cbcc1ebb445f
Kiran <pradhanangakiran@gmail.com>
2017-11-02 22:10:37 +0545

    # Added New Functions   * Draw Circle   * Change Pen Color   * Change Pen Size   * Change Background Color

--- Turts GUI.py
+++ Turts GUI.py
@@ -15,6 +15,7 @@ def move():  # function for turning the turtle
     moveEntry_num = int(moveEntry.get())
     turtle.forward(moveEntry_num)
 
+
 def Pup_Pdn():  #Function for toggle button | toggles penup and pendown
     if Pup_Pdn_button.config('relief')[-1] == 'sunken':
         Pup_Pdn_button.config(relief = "raised")
@@ -26,9 +27,27 @@ def Pup_Pdn():  #Function for toggle button | toggles penup and pendown
         Pup_Pdn_button.config(text="Pen Down")
 
 
+def circle():  # function for turning the turtle
+    circle_size_num = int(circle_size.get())
+    turtle.circle(circle_size_num)
+
+def pencolor():  # function for changing pen color
+    color_text_num = color_text.get()
+    turtle.color(color_text_num)
+
+
+def pensize():  # function for changing pen size
+    pensize_num = int(pensize_input.get())
+    turtle.pen(pensize = pensize_num)
+
+
+def bgcolor():  # function for changing Background color
+    BG_color_text_num = BG_color_text.get()
+    turtle.bgcolor(BG_color_text_num)
+
 window = Tk()
 window.title("Turtle")
-window.geometry("300x300")
+window.geometry("200x400")
 
 heading = Label(window, text="TURTLE GUI").pack()
 
@@ -39,7 +58,7 @@ leftEntry.pack()
 leftEntry_Button = Button(window, text="Turn left", command=turnleft)
 leftEntry_Button.pack()
 
-moveEntry = Entry(window)  # Editbox for left angle
+moveEntry = Entry(window)  # Editbox for move distance
 moveEntry.pack()
 moveEntry_Button = Button(window, text="Move", command=move)
 moveEntry_Button.pack()
@@ -47,5 +66,24 @@ moveEntry_Button.pack()
 Pup_Pdn_button = Button(window, text = "Pen Down", width = 12, relief = "sunken", command = Pup_Pdn)
 Pup_Pdn_button.pack()
 
+circle_size = Entry(window)  # Editbox for circle size
+circle_size.pack()
+circle_size_Button = Button(window, text="Draw Circle", command=circle)
+circle_size_Button.pack()
+
+color_text = Entry(window)  # Editbox for color
+color_text.pack()
+color_text_Button = Button(window, text="Change Color", command=pencolor)
+color_text_Button.pack()
+
+pensize_input = Entry(window)  # Editbox for pen size
+pensize_input.pack()
+pensize_Button = Button(window, text="Change Pensize", command=pensize)
+pensize_Button.pack()
+
+BG_color_text = Entry(window)  # Editbox for color
+BG_color_text.pack()
+BG_color_text_Button = Button(window, text="Change BG Color", command=bgcolor)
+BG_color_text_Button.pack()
 
 window.mainloop()
